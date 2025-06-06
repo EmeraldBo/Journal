c0d56af817f0b23411e7ae8a7442404f87c59bc8
EmeraldBo <xboxisgreat21@yahoo.com>
2025-06-06 17:55:36 +0800

    Testing Notifications

--- netlify/functions/send-notification.js
+++ netlify/functions/send-notification.js
@@ -6,7 +6,7 @@ exports.handler = async function(event, context) {
   const response = await fetch('https://onesignal.com/api/v1/notifications', {
     method: 'POST',
     headers: {
-      'Authorization': 'os_v2_app_dg5bk737unfs7jz7fg2zhsucyfove3mq7iru7uetrqmi2ohs4gdlzvxopmntoa7h4cuufyzbny6qtfjuus4s4oojnnw5vi3liojk63q',
+      'Authorization': 'Basic os_v2_app_dg5bk737unfs7jz7fg2zhsucyfove3mq7iru7uetrqmi2ohs4gdlzvxopmntoa7h4cuufyzbny6qtfjuus4s4oojnnw5vi3liojk63q', // Use 'Basic ' prefix!
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
