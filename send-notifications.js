24423552b7a510de6f1b6f8d249f98ddb80136a7
EmeraldBo <xboxisgreat21@yahoo.com>
2025-06-06 17:08:43 +0800

    Testing Notifications

--- send-notifications.js
+++ send-notifications.js
@@ -6,11 +6,11 @@ exports.handler = async function(event, context) {
   const response = await fetch('https://onesignal.com/api/v1/notifications', {
     method: 'POST',
     headers: {
-      'Authorization': '19ba157f-7fa3-4b2f-a73f-29b593ca82c1',
+      'Authorization': 'os_v2_app_dg5bk737unfs7jz7fg2zhsucyfove3mq7iru7uetrqmi2ohs4gdlzvxopmntoa7h4cuufyzbny6qtfjuus4s4oojnnw5vi3liojk63q',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      app_id: 'YOUR-ONESIGNAL-APP-ID',
+      app_id: '19ba157f-7fa3-4b2f-a73f-29b593ca82c1',
       included_segments: ['All'],
       headings: { en: title },
       contents: { en: message }
